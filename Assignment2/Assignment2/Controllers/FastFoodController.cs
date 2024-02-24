17fa07f1df6784535685fd130363461725af0602
Ozan Lago-Sereflioglu <lago.ozan@gmail.com>
2024-02-23 21:28:43 -0500

    Uploaded Assignment 2

---
+++ Assignment2/Assignment2/Controllers/FastFoodController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class FastFoodController : ApiController
+    {
+        /// <summary>
+        /// J1 - Calculates the total calories of a meal, based on user choices.
+        /// </summary>
+        /// <param name="burger">Burger Choice</param>
+        /// <param name="drink">Drink Choice</param>
+        /// <param name="side">Side Choice</param>
+        /// <param name="dessert">Dessert Choice</param>
+        /// <returns> {burger}+{drink}+{side}+{dessert} </returns>
+        /// <example>
+        /// GET localhost:x/api/J1/Menu/1/2/3/4 -> 691
+        /// </example>
+        [HttpGet]
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        public int Get(int burger, int drink, int side, int dessert)
+        {
+            int[] burgerArray = {461,431,420,0};
+            int[] drinkArray = {130,160,118,0};
+            int[] sideArray = {100,57,70,0};
+            int[] dessertArray = {167,266,75,0};
+
+            int calorieTotal = burgerArray[burger-1]+drinkArray[drink-1]+sideArray[side-1]+dessertArray[dessert-1];
+
+            return calorieTotal;
+        }
+
+    }
+
+}
+
+
---
+++ Assignment2/Assignment2/Controllers/RotatingLettersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class RotatingLettersController : ApiController
+    {
+        /// <summary>
+        /// An artist wants to construct a sign whose letters will rotate freely in the breeze. 
+        /// In order to do this, she must only use letters that are not changed by rotation of 180 degrees: I, O, S, H, Z, X, and N.
+        /// Write a program that reads a word and determines whether the word can be used on the sign.
+        /// </summary>
+        /// <param name="inputWord">User inputted word</param>
+        /// <returns> {"Yes"} || {"No"} </returns>
+        /// <example>
+        /// GET localhost:x/api/J2/RotatingLetters/SHINS -> Yes
+        /// </example>
+                [HttpGet]
+        [Route("api/J2/RotatingLetters/{inputWord}")]
+        public string Get(string inputWord)
+        {
+            string finalOutput = "";
+
+            bool validator = System.Text.RegularExpressions.Regex.IsMatch(inputWord, "[^IOSHZXN]");
+
+            if (validator == false) 
+            {
+            finalOutput = "Yes";
+            }
+
+            else
+            {
+            finalOutput = "No";
+            }
+
+            return finalOutput;
+            }
+        }
+
+    }
+
